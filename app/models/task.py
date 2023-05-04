6afac6d3bef6a35827d96b39f8dfd306c181f92f
Agnes Kim <agneskim4@gmail.com>
2023-05-04 14:11:42 -0400

    created tasks model and updated db

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,9 @@
 from app import db
+from datetime import datetime
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+    task_id = db.Column(db.Integer, primary_key=True, autoincrement = True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    completed_at = db.Column(db.DateTime, nullable = True)
